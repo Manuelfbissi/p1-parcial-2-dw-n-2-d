ee7da548c5861f3844b9dcb723ce9967037de7bf
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2022-11-29 15:36:39 -0300

    funcion

--- index.js
+++ index.js
@@ -39,4 +39,50 @@
           const DOMtotal = document.querySelector('#total');
           const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
-          
+
+
+          // Funciones
+
+        /**
+          * Dibuja todos los productos a partir de la base de datos. 
+          */
+         function renderizarProductos() {
+            baseDeDatos.forEach((info) => {
+                // Estructura
+                const miNodo = document.createElement('div');
+                miNodo.classList.add('card', 'col-sm-4');
+                // Body
+                const miNodoCardBody = document.createElement('div');
+                miNodoCardBody.classList.add('card-body');
+                // Titulo
+                const miNodoTitle = document.createElement('h2');
+                miNodoTitle.classList.add('card-title');
+                miNodoTitle.textContent = info.nombre;
+                // Imagen
+                const miNodoImagen = document.createElement('img');
+                miNodoImagen.classList.add('img-fluid');
+                miNodoImagen.setAttribute('src', info.imagen);
+                // Precio
+                const miNodoPrecio = document.createElement('p');
+                miNodoPrecio.classList.add('card-text');
+                miNodoPrecio.textContent = `${info.precio}${divisa}`;
+                // Boton 
+                const miNodoBoton = document.createElement('button');
+                miNodoBoton.classList.add('btn', 'btn-primary');
+                miNodoBoton.textContent = '+';
+                miNodoBoton.setAttribute('marcador', info.id);
+                miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+               
+                // Insertamos
+                miNodoCardBody.appendChild(miNodoImagen);
+                miNodoCardBody.appendChild(miNodoTitle);
+                miNodoCardBody.appendChild(miNodoPrecio);
+                miNodoCardBody.appendChild(miNodoBoton);
+                miNodo.appendChild(miNodoCardBody);
+                DOMitems.appendChild(miNodo);
+            });
+        }
+
+        
+
+
