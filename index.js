4a58556be874ab8adba2d413f134161f734095f2
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2022-11-29 15:39:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', () => {
     // Variables
     const baseDeDatos = [
         {
@@ -93,5 +93,89 @@
             renderizarCarrito();
 
         }
+
+        /**
+          * Dibuja todos los productos guardados en el carrito
+          */
+         function renderizarCarrito() {
+            // Vaciamos todo el html
+            DOMcarrito.textContent = '';
+            // Quitamos los duplicados
+            const carritoSinDuplicados = [...new Set(carrito)];
+            // Generamos los Nodos a partir de carrito
+            carritoSinDuplicados.forEach((item) => {
+                // Obtenemos el item que necesitamos de la variable base de datos
+                const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                    // ¿Coincide las id? Solo puede existir un caso
+                    return itemBaseDatos.id === parseInt(item);
+                });
+                // Cuenta el número de veces que se repite el producto
+                const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                    // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                    return itemId === item ? total += 1 : total;
+                }, 0);
+                // Creamos el nodo del item del carrito
+                const miNodo = document.createElement('li');
+                miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+                miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+                // Boton de borrar
+                const miBoton = document.createElement('button');
+                miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+                miBoton.textContent = 'X';
+                miBoton.style.marginLeft = '1rem';
+                miBoton.dataset.item = item;
+                miBoton.addEventListener('click', borrarItemCarrito);
+                // Mezclamos nodos
+                miNodo.appendChild(miBoton);
+                DOMcarrito.appendChild(miNodo);
+            });
+           // Renderizado de el precio total en el HTML
+           DOMtotal.textContent = calcularTotal();
+        }
+
+        /**
+        * borrar un elemento del carrito
+        */
+        function borrarItemCarrito(evento) {
+            //ID que hay en el boton pulsado
+            const id = evento.target.dataset.item;
+            // Borrar TODOS los productos 
+            carrito = carrito.filter((carritoId) => {
+                return carritoId !== id;
+            });
+            // renderizar
+            renderizarCarrito();
+        }
+
+        /**
+         * Calcula el precio total teniendo en cuenta los productos repetidos
+         */
+        function calcularTotal() {
+            // Recorremos el array del carrito 
+            return carrito.reduce((total, item) => {
+                // De cada elemento obtenemos su precio
+                const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                    return itemBaseDatos.id === parseInt(item);
+                });
+                // Los sumamos al total
+                return total + miItem[0].precio;
+            }, 0).toFixed(2);
+        }
+
+        
+        function vaciarCarrito() {
+            // Limpiamos los productos guardados
+            carrito = [];
+            // Renderizamos los cambios
+            renderizarCarrito();
+        }
+
+        // Eventos
+        DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+        // Inicio
+        renderizarProductos();
+        renderizarCarrito();
+      });
 
 
