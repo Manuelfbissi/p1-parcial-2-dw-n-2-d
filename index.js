40ffbf8dcc49c3af7c86574baa087b2bc31aee37
Manuel Bissi <manuel.fernandez@davinci.edu.ar>
2022-11-29 15:33:19 -0300

    html-variables

---
+++ index.js
@@ -0,0 +1,33 @@
+
+    // Variables
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'Caramelo',
+            descripcion: 'Descripción del producto',
+            precio: 5,
+            imagen: 'imagen'
+        },
+        {
+            id: 2,
+            nombre: 'Chicle',
+            descripcion: 'Descripción del producto',
+            precio: 4,
+            imagen: 'imagen'
+        },
+        {
+            id: 3,
+            nombre: 'Chupetin',
+            descripcion: 'Descripción del producto',
+            precio: 1,
+            imagen: 'imagen'
+        },
+        {
+            id: 4,
+            nombre: 'Alfajor',
+            descripcion: 'Descripción del producto',
+            precio: 3,
+            imagen: 'imagen'
+        }
+
+    ];
